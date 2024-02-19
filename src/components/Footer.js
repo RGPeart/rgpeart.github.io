7bfd163e7456ccda7e2e5ea0969b9c2c4589fd9a
Ryan Peart <rgpeart@hotmail.com>
2024-02-19 14:09:02 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Soumyajit Behera</h3>
+          <h3>Designed and Developed by Ryan Peart</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} SB</h3>
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/RGPeart"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -31,6 +31,7 @@ function Footer() {
                 <AiFillGithub />
               </a>
             </li>
+            /*
             <li className="social-icons">
               <a
                 href="https://twitter.com/Soumyajit4419"
@@ -41,9 +42,10 @@ function Footer() {
                 <AiOutlineTwitter />
               </a>
             </li>
+            */
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://www.linkedin.com/in/ryan-peart/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -53,7 +55,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/soumyajit4419"
+                href="https://www.instagram.com/RGPeart"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
